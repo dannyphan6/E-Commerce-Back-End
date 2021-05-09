aa1303084ddcd0f927520c878130b0debd5f4635
Daniel Phan <dannyphan@comcast.net>
2021-05-08 23:28:43 -0700

    Created delete route for tags and tested successfully

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,6 +51,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
+    res.json(data)
+  }).catch((err) => {
+    res.json(err)
+  })
 });
 
 module.exports = router;
